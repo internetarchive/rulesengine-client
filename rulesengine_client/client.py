84be3e31b2fb559a7c76dc6cb7233bcee7cc28a0
Barbara Miller <barbara@archive.org>
2021-07-20 21:36:52 +0000

    client.py updates for direct postgres query

--- rulesengine_client/client.py
+++ rulesengine_client/client.py
@@ -1,7 +1,10 @@
 import logging
+import psycopg2
+from psycopg2 import extras
 import requests
 
 from .response import Response
+from .models import RuleCollection
 
 
 class Client(object):
@@ -52,3 +55,50 @@ class Client(object):
         response = requests.get(
             '{}/rules/for-request'.format(self.host), params=p)
         return Response(response)
+
+    def rules_from_postgres(self, surt, capture_date, neg_surt=None,
+                            collection=None, partner=None):
+        query_start = ('SELECT policy, surt, '
+                       'capture_date_start, capture_date_end, '
+                       'collection, partner, '
+                       'rewrite_from, rewrite_to, '
+                       'warc_match, neg_surt, '
+                       'retrieve_date_start, retrieve_date_end, '
+                       'seconds_since_capture, '
+                       'ip_range_start, ip_range_end, '
+                       'environment, protocol '
+                       'from rules_rule where %s like surt and enabled = true'
+                       )
+        if collection:
+            query_end = ' and collection = %s;'
+            who = collection
+        elif partner:
+            query_end = ' and partner = %s;'
+            who = partner
+        else:
+            query_end = ';'
+            who = None
+        rules_query = ''.join([query_start, query_end])
+        try:
+            conn = psycopg2.connect(
+                    'host=db.qa-archive-it.org dbname=rulesengine user=archiveit',
+                    cursor_factory=extras.DictCursor
+                    )
+        except Exception as e:
+            self._log.warning(f'db connection failure: {e}')
+            return None
+        cur = conn.cursor()
+        try:
+            if collection or partner:
+                cur.execute(rules_query, (surt, who, ))
+            else:
+                cur.execute(rules_query, (surt, ))
+        except Exception as e:
+            self._log.warning(f'exception querying for {surt} and {who}: {e}')
+            return None
+        rules = cur.fetchall()
+        if rules:
+            self._log.debug('returning {}...'.format(rules[0]))
+        else:
+            self._log.debug('no rules returned')
+        return RuleCollection.from_pg_response(rules)
